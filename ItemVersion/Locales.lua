4926092405db471338de392308e2ca873be9dcc0
Tim Martin <tim@timmart.in>
2026-02-07 18:30:17 -0600

    wip

---
+++ ItemVersion/Locales.lua
@@ -0,0 +1,44 @@
+local AddonName = ...
+
+local L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "enUS", true)
+--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
+
+L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "deDE")
+if not L then return end
+--@localization(locale="deDE", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
+
+L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "esES")
+if not L then return end
+--@localization(locale="esES", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
+
+L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "esMX")
+if not L then return end
+--@localization(locale="esMX", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
+
+L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "frFR")
+if not L then return end
+--@localization(locale="frFR", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
+
+L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "itIT")
+if not L then return end
+--@localization(locale="itIT", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
+
+L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "koKR")
+if not L then return end
+--@localization(locale="koKR", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
+
+L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "ptBR")
+if not L then return end
+--@localization(locale="ptBR", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
+
+L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "ruRU")
+if not L then return end
+--@localization(locale="ruRU", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
+
+L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "zhCN")
+if not L then return end
+--@localization(locale="zhCN", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
+
+L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "zhTW")
+if not L then return end
+--@localization(locale="zhTW", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@

09aab4e352526eef4ce2ac812d85bcd2d467bf6e
Tim Martin <tim@timmart.in>
2026-07-16 18:51:52 -0500

    table: delete unused DeepCopy
    Closes #111

--- ItemVersion/Table.lua
+++ ItemVersion/Table.lua
@@ -1,25 +1,5 @@
 local _, Private = ...
 
----Creates a deep copy of a table
----Note: Does not handle cyclic references
----@generic T
----@param obj T The object to copy
----@return T copy The deep copy of the object
-local function deepCopy(obj)
-  -- Does not handle cyclic references
-  if type(obj) ~= "table" then
-    return obj
-  end
-
-  local res = {}
-  for k, v in pairs(obj) do
-    -- Recursively copy the key and the value
-    res[deepCopy(k)] = deepCopy(v)
-  end
-
-  return res
-end
-
 ---Mixes in properties from one or more mixin tables into a target object
 ---@generic T
 ---@param object T The target object to receive the mixin properties
@@ -48,7 +28,6 @@ local function keepOnlyKnownKeys(t, allowed)
 end
 
 Private.Table = {
-  DeepCopy = deepCopy,
   Mixin = mixin,
   KeepOnlyKnownKeys = keepOnlyKnownKeys,
 }
